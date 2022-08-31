731d684271c201989585e61cfdaad8fc3aeaa2b2
rickyurvina <ricky_uc12@hotmail.com>
2022-08-31 13:27:18 -0400

    Initial commit

---
+++ app/src/main/java/com/example/clase3108/ThirdFragment.java
@@ -0,0 +1,37 @@
+package com.example.clase3108;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.clase3108.databinding.FragmentThirdBinding;
+
+public class ThirdFragment extends Fragment {
+
+    private FragmentThirdBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = FragmentThirdBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
