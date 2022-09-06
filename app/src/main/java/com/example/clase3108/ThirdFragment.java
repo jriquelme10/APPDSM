c4033925ad546965861c33bf368de4388e0e6a4a
rickyurvina <ricky_uc12@hotmail.com>
2022-09-06 11:45:59 -0300

    Navigation fragments

--- app/src/main/java/com/example/clase3108/ThirdFragment.java
+++ app/src/main/java/com/example/clase3108/ThirdFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,6 +27,20 @@ public class ThirdFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.btnToSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ThirdFragment.this)
+                        .navigate(R.id.action_thirdFragment_to_SecondFragment);
+            }
+        });
+        binding.btnToFirstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ThirdFragment.this)
+                        .navigate(R.id.action_thirdFragment_to_FirstFragment);
+            }
+        });
     }
 
     @Override
